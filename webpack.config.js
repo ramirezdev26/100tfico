58b39fce084f9d85605efeaa2f38d1e24f593f13
ramirezdev26 <santiago.ramirezc26@gmail.com>
2022-06-15 19:00:29 -0500

    Created utils for the SPA to manage the hash in routes, resolve routes and get data from API. Also implementation in pages Home and Characters

--- webpack.config.js
+++ webpack.config.js
@@ -29,12 +29,12 @@ module.exports = {
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin([
+    new HtmlWebpackPlugin(
       {
         inject: true,
         template: './public/index.html',
         filename: './index.html',
       }
-    ]),
+    ),
   ]
 }
